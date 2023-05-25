6230e64a0a3e3a9d852afe96d93915bf880b3864
NJ9802 <nelsonjavier9802@gmail.com>
2023-05-25 19:34:51 -0400

    Add logo Image to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import DropdownMenu from "./DropdownMenu";
+import Image from "next/image";
 
 type Props = {};
 
@@ -9,6 +10,13 @@ export default function Navbar({}: Props) {
     <header className="navbar sticky top-0 bg-primary text-primary-content z-50">
       <div className="flex-1">
         <Link href="/" className="btn btn-ghost normal-case text-xl">
+          <Image
+            className="mr-2 rounded-lg"
+            width={48}
+            height={48}
+            src={"/android-chrome-192x192.png"}
+            alt="CombusTank logo"
+          />
           CombusTank
         </Link>
       </div>
