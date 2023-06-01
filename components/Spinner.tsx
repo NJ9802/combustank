73e66ba203ddbea817825565ae51eec69d4cfca7
NJ9802 <nelsonjavier9802@gmail.com>
2023-06-01 17:05:36 -0400

    Change spinner component

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -5,14 +5,7 @@ type Props = {};
 export default function Spinner({}: Props) {
   return (
     <div className="pt-0 flex flex-1 justify-center items-center">
-      <div
-        className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
+      <span className="loading loading-spinner loading-lg"></span>{" "}
     </div>
   );
 }
